4210d4adff9448fb1060123829dc6038989d1a58
ym1522 <ym1522@naver.com>
2022-11-07 17:40:27 +0900

    bj 10867 solution

---
+++ Baekjoon/10867_bj.py
@@ -0,0 +1,13 @@
+# 10867 - 중복 빼고 정렬하기
+# 메모리: 40596KB, 시간: 112ms
+import sys
+n = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+num = list(set(num))
+num.sort()
+print(*num, sep=' ')
+
+'''
+sort하고 set하면 틀림
+왜 ??
+'''
